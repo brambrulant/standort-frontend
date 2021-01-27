f6c2d26b8cc9253d438d25cc9d627942893a5c74
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-27 16:49:25 +0100

    store rich text editor as md

--- src/pages/MainPage/Post.js
+++ src/pages/MainPage/Post.js
@@ -1,15 +1,55 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import Comment from "./Comment";
+import ReactMarkdown from "react-markdown";
 
 export default function Post({ post }) {
+  // return (
+  //   <div>
+  //     <h1>date: {post.createdAt}</h1>
+  //     <h2>location: {post.location}</h2>
+  //     <p>message {post.message}</p>
+  //     <p>created by {post.user}</p>
+  //     {post.comments &&
+  //       post.comments.map((comment) => <Comment key={comment.id} comment={comment} />)}
+  //   </div>
+  // );
   return (
-    <div>
-      <h1>date: {post.createdAt}</h1>
-      <h2>location: {post.location}</h2>
-      <p>message {post.message}</p>
-      <p>created by {post.user}</p>
-      {post.comments &&
-        post.comments.map((comment) => <Comment key={comment.id} comment={comment} />)}
-    </div>
+    <Card>
+      <ReactMarkdown>
+        <CardActionArea>
+          {post.image ? (
+            <CardMedia image={post.image} title={`image of ${post.name}`} />
+          ) : null}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {post.title}
+            </Typography>
+            <Typography gutterBottom variant="body2" component="p">
+              {post.title}
+            </Typography>
+            <Typography variant="body1" color="textSecondary" component="p">
+              {post.message}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          {/* <Button size="small" color="primary">
+          {post.comments.length}
+        </Button> */}
+          {/* <Button size="small" color="primary">
+          Learn More
+        </Button> */}
+        </CardActions>
+      </ReactMarkdown>
+    </Card>
   );
 }
