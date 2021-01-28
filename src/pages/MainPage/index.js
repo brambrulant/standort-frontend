1d4bed8a4ca367939332c7d773062f4bacf3bc08
Milo Silva <milo.silva234@gmail.com>
2021-01-28 18:03:06 +0100

    Create Post button now disabled if no user present

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,13 +1,13 @@
 import CreatePost from "../../components/CreatePost/CreatePost";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Grid, Modal, Paper } from "@material-ui/core";
-import { getMyLocationName } from "../../store/location/actions";
+import { Button, Grid, Modal } from "@material-ui/core";
 import { selectMyLocation } from "../../store/location/selector";
 import { fetchPostsWithMyLocation } from "../../store/posts/actions";
 import { selectPosts } from "../../store/posts/selectors";
 import Post from "./Post";
 import "./index.css";
+import { selectToken } from "../../store/user/selector";
 
 export default function MainPage() {
   const dispatch = useDispatch();
@@ -18,6 +18,7 @@ export default function MainPage() {
   // redux selectors
   const location = useSelector(selectMyLocation);
   const posts = useSelector(selectPosts);
+  const userToken = useSelector(selectToken);
 
   useEffect(() => {
     if (location) dispatch(fetchPostsWithMyLocation(location));
@@ -27,7 +28,9 @@ export default function MainPage() {
     <>
       <div className="container">
         <h2>{location}</h2>
-        <Button onClick={() => setCPVisibility(true)}>Create Post</Button>
+        <Button onClick={() => setCPVisibility(true)} disabled={!userToken}>
+          Create Post
+        </Button>
         <div className="posts">
           <Grid container spacing={3}>
             {posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />)}
