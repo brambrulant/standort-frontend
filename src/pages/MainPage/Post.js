3953a7006604851162a4ff6d03af9616e1f04b18
Milo Silva <milo.silva234@gmail.com>
2021-01-29 15:36:17 +0100

    Restyle Post - Switch username & title elements - resize user avatar to match smaller font size - change the color of the message font to a darker shade

--- src/pages/MainPage/Post.js
+++ src/pages/MainPage/Post.js
@@ -5,7 +5,9 @@ import {
   CardActions,
   CardContent,
   Chip,
-  Grid, GridListTile,
+  Grid,
+  GridListTile,
+  makeStyles,
   Typography,
 } from "@material-ui/core";
 import ReadMoreReact from "read-more-react";
@@ -13,14 +15,23 @@ import React from "react";
 import CommentSection from "./CommentSection";
 import "./Post.css";
 import tagList from "../../config/tags";
+import { formatDistance } from "date-fns";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
 
 export default function Post({ post }) {
-  const { comments, id, tags } = post;
-
+  const { comments, id, tags, createdAt } = post;
+  const classes = useStyles();
   const cardStyle = {
     // display: "block",
-     width: "350px",
-    margin: '10px',
+    width: "350px",
+    margin: "10px",
     // // height: "30vw",
     // overflow: "auto",
   };
@@ -30,7 +41,7 @@ export default function Post({ post }) {
     return (
       <Chip
         color="primary"
-        style={{marginTop: "10px", marginBottom: "10px", marginRight: "10px"}}
+        style={{ marginTop: "10px", marginBottom: "10px", marginRight: "10px" }}
         key={i}
         label={tagName}
         variant="default"
@@ -41,41 +52,41 @@ export default function Post({ post }) {
   });
 
   return (
-      <Card style={cardStyle}>
-        <CardActionArea>
-          <CardContent>
-            <div className="post-title">
-              <Avatar alt={post.user.name} src={post.user.profilePic} />
-              <Typography gutterBottom variant="h5" component="h2" className="post-user-name">
-                {post.user.name}
-              </Typography>
-            </div>
+    <Card style={cardStyle}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="h2" className="post-user-name">
+            {post.title}
+          </Typography>
+          <div className="post-title">
+            <Avatar className={classes.avatar} alt={post.user.name} src={post.user.profilePic} />
             <Typography gutterBottom variant="body2" component="p">
-              {post.title}
+              {post.user.name}, {formatDistance(new Date(createdAt), new Date())} ago
             </Typography>
-            <Typography variant="body1" color="textSecondary" component="div">
-              <ReadMoreReact text={post.message} readMoreText="read more..." />
-            </Typography>
-            {post.picture ? (
-              <img
-                src={post.picture}
-                alt={`${post.name}`}
-                style={{
-                  maxHeight: "200px",
-                  maxWidth: "200px",
-                  display: "block",
-                  borderRadius: "10px",
-                }}
-              />
-            ) : null}
-            {tagChips}
-            <CardActions>
-              <Grid>
-                <CommentSection postId={id} comments={comments} alignSelf="flex-end" />
-              </Grid>
-            </CardActions>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+          </div>
+          <Typography variant="body1" color="default" component="div">
+            <ReadMoreReact text={post.message} readMoreText="read more..." />
+          </Typography>
+          {post.picture ? (
+            <img
+              src={post.picture}
+              alt={`${post.name}`}
+              style={{
+                maxHeight: "200px",
+                maxWidth: "200px",
+                display: "block",
+                borderRadius: "10px",
+              }}
+            />
+          ) : null}
+          {tagChips}
+          <CardActions>
+            <Grid>
+              <CommentSection postId={id} comments={comments} alignSelf="flex-end" />
+            </Grid>
+          </CardActions>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
