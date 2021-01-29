2f0d8d51f826f69cd263a2c41fdb340a716aacf4
Milo Silva <milo.silva234@gmail.com>
2021-01-29 13:29:08 +0100

    Add location with icon & styling to NavBar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,10 +7,13 @@ import { selectToken } from "../../store/user/selector";
 import NavbarItem from "./NavbarItem";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
+import { selectMyLocation } from "../../store/location/selector";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import { Typography } from "@material-ui/core";
 
 export default function Navigation() {
   const token = useSelector(selectToken);
-
+  const location = useSelector(selectMyLocation);
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
 
   return (
@@ -21,7 +24,12 @@ export default function Navigation() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
-          <Nav.Item></Nav.Item>
+          <Nav.Item>
+            <Typography variant="h6" component="span">
+              <LocationOnIcon />
+              {location}
+            </Typography>
+          </Nav.Item>
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
