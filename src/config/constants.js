75b4ffa932915a99031ba02cb9579cb2214aaf17
Milo Silva <milo.silva234@gmail.com>
2021-02-01 16:05:56 +0100

    Add Heroku url to use production API during deployment

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,6 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://cairn-api.herokuapp.com/"
+    : "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const ACCESS_KEY = "32a9e8a6c631e9b354fecd17ee97477f";
-export const tags = [
-  "Museum",
-  "Outdoors",
-  "Wildlife",
-  "Shopping",
-  "Restaurant",
-  "Nightlife",
-  "Spa",
-];
